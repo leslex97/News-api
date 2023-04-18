11fcc2c9dca06c677ee8c21a841a79ac7bcccf23
Pawel Woroniecki <pawelwr.121@gmail.com>
2023-04-18 22:24:06 +0200

    Building first Angular project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'woroniecki-pawel-pr-08'`, () => {
+  it(`should have as title 'woroniecki-pawel-pr08'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('woroniecki-pawel-pr-08');
+    expect(app.title).toEqual('woroniecki-pawel-pr08');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('woroniecki-pawel-pr-08 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('woroniecki-pawel-pr08 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'woroniecki-pawel-pr-08';
+  title = 'woroniecki-pawel-pr08';
 }
